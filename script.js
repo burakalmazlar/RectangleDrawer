4dc8c865e5cc58fd64c944ef019bd8288f80124e
BURAK ALMAZLAR <burak.almazlar@epias.com.tr>
2023-03-07 02:50:27 +0300

    working to resize from all directions

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 (function () {
   function init(event) {
-    RectangleDrawer.addRectangler(document.body, 300);
-    RectangleDrawer.addRectangler(document.body, 300);
+    RectangleDrawer.addRectangler(document.body, 700);
   }
 
   document.addEventListener("DOMContentLoaded", init);
@@ -16,8 +15,8 @@ var RectangleDrawer = {
     element.appendChild(div);
 
     var gapX, gapY; // to use event.x and event.y
-    var firstTop, firstLeft; // initial drawing point
-    var pickedTop, pickedLeft; // picked point
+    var firstY, firstX; // initial drawing point
+    var pickedY, pickedX; // picked point
     var rectangle = undefined; // rectangle to be drawn
     var text = undefined; // info text at top left of rectangle
     var picked = false; // rectangle picked for moving
@@ -26,6 +25,7 @@ var RectangleDrawer = {
     var width, height; // rectangle current size
     var newTop, newLeft; // position while moving
     var resizing = 0;
+    const sensitivity = 10;
 
     function createRect() {
       if (!rectangle) {
@@ -37,7 +37,8 @@ var RectangleDrawer = {
         text.className = "info";
         div.appendChild(text);
 
-        draw(firstLeft, firstTop);
+        console.log("--> Create Rect <--");
+        draw(firstX, firstY);
 
         rectangle.addEventListener("mousedown", onRectMouseDown);
       }
@@ -51,34 +52,91 @@ var RectangleDrawer = {
       }
     }
 
+    var firstHeight, firstWidth;
     function draw(x, y) {
       if (rectangle) {
-        var yDiff = y - firstTop;
-        var xDiff = x - firstLeft;
+        var xDiff = x - firstX;
+        var yDiff = y - firstY;
+
         var xDiffLen = Math.abs(xDiff);
         var yDiffLen = Math.abs(yDiff);
 
-        if (resizing != 2) { // if not vertical scaling
-          top = yDiff < 0 ? firstTop - yDiffLen : firstTop;
-          top = top < 0 ? 0 : top;
-          rectangle.style.top = top + "px";
+        console.log({ xDiff, yDiff, resizing });
 
-          height = top + yDiffLen > size ? size - 2 - top : yDiffLen;
-          height = y < 0 ? firstTop : height;
-          rectangle.style.height = height + "px";
+        if (resizing > 0) {
+          switch (resizing) {
+            case 1:
+              if (yDiff < firstHeight) {
+                top = y;
+                height = firstHeight - yDiff;
+              } else {
+                top = firstY + firstHeight;
+                height = yDiff - firstHeight;
+              }
+
+              if (xDiff < firstWidth) {
+                left = x;
+                width = firstWidth - xDiff;
+              } else {
+                left = firstX + firstWidth;
+                width = xDiff - firstWidth;
+              }
+
+              break;
+            case 2:
+              break;
+            case 3:
+              break;
+            case 4:
+              break;
+            case 5:
+              if (yDiff < 0 && yDiffLen > firstHeight) {
+                top = y;
+                height = yDiffLen - firstHeight;
+              } else {
+                height = firstHeight + yDiff;
+              }
+
+              if (xDiff < 0 && xDiffLen > firstWidth) {
+                left = x;
+                width = xDiffLen - firstWidth;
+              } else {
+                width = firstWidth + xDiff;
+              }
+              break;
+            case 6:
+              break;
+            case 7:
+              break;
+            case 8:
+              break;
+
+            default:
+              break;
+          }
+        } else {
+          top = yDiff > 0 ? firstY : y;
+          left = xDiff > 0 ? firstX : x;
+
+          height = yDiffLen;
+          width = xDiffLen;
         }
 
-        if (resizing != 3) { // if not horizontal scaling
-          left = xDiff < 0 ? firstLeft - xDiffLen : firstLeft;
-          left = left < 0 ? 0 : left;
-          rectangle.style.left = left + "px";
+        top = top < 0 ? 0 : top;
+        rectangle.style.top = top + "px";
 
-          width = left + xDiffLen > size ? size - 2 - left : xDiffLen;
-          width = x < 0 ? firstLeft : width;
-          rectangle.style.width = width + "px";
-        }
+        height = y < 0 ? firstY : height;
+        height = top + height > size ? size - 2 - top : height;
+        rectangle.style.height = height + "px";
 
-        text.innerHTML = xDiffLen + " x " + yDiffLen;
+        left = left < 0 ? 0 : left;
+        rectangle.style.left = left + "px";
+
+        width = x < 0 ? firstX : width;
+        width = left + width > size ? size - 2 - left : width;
+        rectangle.style.width = width + "px";
+
+        text.innerHTML = height + " x " + width;
         alignTextPosition();
       }
     }
@@ -90,51 +148,42 @@ var RectangleDrawer = {
 
       if (resizing == 0) {
         picked = true;
-        pickedTop = y;
-        pickedLeft = x;
+        pickedY = y;
+        pickedX = x;
       } else {
         drawing = true;
       }
-
     };
 
-    function getResizing(x, y) {
-      var _resizing = 0;
-      var _top = gapY + top;
-      var _left = gapX + left;
-      var bottom = _top + height;
-      var right = _left + width;
-      if ((x + 8 > right && y > _top)) {
-        _resizing = 2; // horizontal
-      }
-      if ((y + 8 > bottom && x > _left)) {
-        _resizing = 3; // vertical
-      }
-      if (y + 8 > bottom && x + 8 > right) {
-        _resizing = 1; // both
-      }
-      return _resizing;
-    }
-
     var onAreaMouseDown = function (event) {
+      const x = event.x;
+      const y = event.y;
+
+      if (!gapX && !gapY) {
+        gapX = x - event.offsetX;
+        gapY = y - event.offsetY;
+        console.log({ gapX, gapY });
+      }
+
+      firstX = x - gapX;
+      firstY = y - gapY;
+      console.log({ firstX, firstY });
+
       if (!drawing && !picked && resizing == 0) {
         clearRect();
-        gapX = event.x - event.offsetX;
-        gapY = event.y - event.offsetY;
-        console.log("gapY=" + gapY + " gapX=" + gapX)
-        firstLeft = event.x - gapX;
-        firstTop = event.y - gapY;
         createRect();
         drawing = true;
       }
-    }
+    };
 
     function move(x, y) {
-      newTop = top + y - pickedTop;
+      newTop = top + y - pickedY;
+      newLeft = left + x - pickedX;
+
       if (newTop > -1 && newTop + height < size) {
         rectangle.style.top = newTop + "px";
       }
-      newLeft = left + x - pickedLeft;
+
       if (newLeft > -1 && newLeft + width < size) {
         rectangle.style.left = newLeft + "px";
       }
@@ -164,30 +213,58 @@ var RectangleDrawer = {
       }
     };
 
-    function clearResizeCursor() {
-      rectangle.classList.remove("resize1");
-      rectangle.classList.remove("resize2");
-      rectangle.classList.remove("resize3");
-    }
-
     var onDocumentMouseUp = function (event) {
       if (picked) {
         top = newTop;
         left = newLeft;
-        firstTop = top;
-        firstLeft = left;
+        firstY = top;
+        firstX = left;
       }
       if (drawing) {
-        firstTop = top;
-        firstLeft = left;
+        firstY = top;
+        firstX = left;
+        firstHeight = height;
+        firstWidth = width;
       }
       drawing = false;
       picked = false;
       resizing = 0;
+    };
+
+    function clearResizeCursor() {
+      for (let index = 1; index <= 8; index++) {
+        rectangle.classList.remove("resize" + index);
+      }
+    }
+
+    function getResizing(x, y) {
+      var _resizing = 0;
+
+      var _top = gapY + top;
+      var _left = gapX + left;
+      var _bottom = _top + height;
+      var _right = _left + width;
+
+      const nearTop = y < _top + sensitivity;
+      const nearRight = x > _right - sensitivity;
+      const nearBottom = y > _bottom - sensitivity;
+      const nearLeft = x < _left + sensitivity;
+
+      if (nearTop) _resizing = 2;
+      if (nearRight) _resizing = 4;
+      if (nearBottom) _resizing = 6;
+      if (nearLeft) _resizing = 8;
+
+      if (nearTop && nearLeft) _resizing = 1;
+      if (nearTop && nearRight) _resizing = 3;
+      if (nearBottom && nearRight) _resizing = 5;
+      if (nearBottom && nearLeft) _resizing = 7;
+
+      return _resizing;
     }
 
     div.addEventListener("mousedown", onAreaMouseDown);
     document.addEventListener("mouseup", onDocumentMouseUp);
     document.addEventListener("mousemove", onDocumentMouseMove);
-  }
+  },
 };
